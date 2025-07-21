9df15f55fda4e7a3c5e2d987e17c39853fb4a11e
ozspeech <nghiahuynh.aiengr@gmail.com>
2025-07-21 19:56:22 +0700

    update

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import gradio as gr
+import datetime
 
 from app import MOSApp
 
@@ -9,6 +10,12 @@ app = FastAPI()
 async def root():
     return 'Gradio app is running at /gradio', 200
 
-demo = MOSApp().create_interface()
+current_date = datetime.now().strftime("%Y%m%d")
+app = MOSApp(
+        dirpath="./samples/data",   # change as you need
+        outdir=f"./results/{current_date}",
+        progress_dir=f"./progress/{current_date}",
+    )
+demo = app.create_interface()
 
 app = gr.mount_gradio_app(app, demo, path='/gradio')
